ceb479627c6709fe3355ec127dc76699df4d8bae
SimeonPetr0v <simeonp925@gmail.com>
2024-03-29 23:02:20 +0200

    Homework

---
+++ Extract Emails/Extract Emails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+
+            string path = @"\b[A-Za-z0-9]+(?:[_\-\.][A-Za-z0-9]+)*@[A-Za-z0-9]+(?:[A-Za-z\-][A-Za-z0-9]+)*\.[A-Za-z]{2,}(?:\.[A-Za-z]{2,})?\b";
+            Regex regex = new Regex(path);
+            MatchCollection matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
